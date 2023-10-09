690577c674273de33a9df794908b42c6f60fbd97
Robin Lemmers <38210@ma-web.nl>
2023-10-09 11:51:04 +0200

    haiku1

--- haiku.js
+++ haiku.js
@@ -1,11 +1,11 @@
 
 
 //plak hier de haiku functions onder elkaar
-function HaikuX(){
-    console.log("haiku by:","AUTHOR");
-    console.log("REGEL1");
-    console.log("REGEL2");
-    console.log("REGEL3");
+function Haiku1(){
+    console.log("haiku by:","Matsuo Bashō");
+    console.log("From time to time ");
+    console.log("The clouds give rest ");
+    console.log("To the moon-beholders.");
 }
 function HaikuX(){
     console.log("haiku by:","AUTHOR");
