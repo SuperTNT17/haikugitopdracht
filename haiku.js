31c9477c47fb5ed6f7c0b969117f81c2c7bb125d
Robin Lemmers <38210@ma-web.nl>
2023-10-09 11:45:24 +0200

    added haiku functions

--- haiku.js
+++ haiku.js
@@ -1,7 +1,24 @@
 
 
 //plak hier de haiku functions onder elkaar
-
+function HaikuX(){
+    console.log("haiku by:","AUTHOR");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+function HaikuX(){
+    console.log("haiku by:","AUTHOR");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+function HaikuX(){
+    console.log("haiku by:","AUTHOR");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
 
 
 
